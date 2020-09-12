9cf278d0e88032300518adfe9e29573cdee754f6
luckilym <50648331+luckilym@users.noreply.github.com>
2020-09-12 20:34:06 +0800

    前端

---
+++ src/pages/Sport.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="sports">
+		<panel-list :sport-list="sportsList"></panel-list>
+	</div>
+</template>
+
+<script>
+import panelList from "@/components/panelList";
+import data from '../../jsonData/sportList'
+import { mapGetters } from "vuex";
+
+export default {
+  components: {
+    panelList
+  },
+  data() {
+
+    return {
+      sportsList:data.data
+    };
+  },
+  created() {
+    if (this.sportsList.length == 0) {
+      this.$store.dispatch("getSportsList");
+    }
+  },
+  computed: {
+    // ...mapGetters(["sportsList"])
+  },
+  mounted() {},
+  methods: {}
+};
+</script>
---
+++ src/pages/Travel.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="travels">
+		<travel-list :travel-lists="travelsList"></travel-list>
+	</div>
+</template>
+
+<script>
+import travelList from "@/components/travelList";
+
+import { mapGetters } from "vuex";
+import data from '../../jsonData/travelList'
+export default {
+  components: {
+    travelList
+  },
+  created() {
+    if (this.travelsList.length == 0) {
+      this.$store.dispatch("getTravelsList");
+    }
+  },
+  computed: {
+    // ...mapGetters(["travelsList"])
+  },
+  data() {
+    return {
+      travelsList:data.data,
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.travels {
+  padding-top: 1px;
+}
+</style>
